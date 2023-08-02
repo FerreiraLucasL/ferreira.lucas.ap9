1b1c47f36b12826cf67c96a2b3a2106f2ad36d5e
Ferreira Lucas <lukasm4a1@gmail.com>
2023-08-02 12:57:00 -0300

    clase cliente+repo abstracto

--- src/main/java/com/mindhub/homebanking/HomebankingApplication.java
+++ src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -1,7 +1,11 @@
 package com.mindhub.homebanking;
 
+import com.mindhub.homebanking.models.Client;
+import com.mindhub.homebanking.repositories.ClientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HomebankingApplication {
@@ -9,6 +13,12 @@ public class HomebankingApplication {
 	public static void main(String[] args) {
 		//
 		SpringApplication.run(HomebankingApplication.class, args);
-
+	}
+	@Bean
+	public CommandLineRunner initData(ClientRepository clientRepository){
+		return (args) -> {
+			Client client = new Client("1231514","Jorge","delSoto" );
+			clientRepository.save(client);
+		};
 	}
 }
