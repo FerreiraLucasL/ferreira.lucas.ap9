1a60694f4a4eb070466287521d5a3a9097e6e80a
Ferreira Lucas <lukasm4a1@gmail.com>
2023-09-14 11:56:27 -0300

    deploy try 3 corrected some Random() issues

--- src/main/java/com/mindhub/homebanking/utils/AccountUtils.java
+++ src/main/java/com/mindhub/homebanking/utils/AccountUtils.java
@@ -2,9 +2,7 @@ package com.mindhub.homebanking.utils;
 
 import com.mindhub.homebanking.repositories.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import java.util.Random;
-
 public class AccountUtils {
     @Autowired static AccountRepository accountRepository;
     public static String createAccountNumber(){
